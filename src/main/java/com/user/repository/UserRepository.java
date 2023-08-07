d6db5db18b9ce4cf5c706efac21d1742a4296eb7
sanketschendake <76549372+sanketschendake@users.noreply.github.com>
2023-08-07 15:00:49 +0530

    Add files via upload

---
+++ src/main/java/com/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.user.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.user.model.User;
+import java.util.List;
+
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+	
+	User findByEmail(String email);
+}
