c3c1b2d5135f2c5dc4bd9a2da90c1235e98e5a0f
sanketschendake <76549372+sanketschendake@users.noreply.github.com>
2023-08-09 12:06:38 +0530

    Add files via upload

---
+++ Spring-Rest-Api/src/main/java/com/springrest/service/CourseService.java
@@ -0,0 +1,21 @@
+package com.springrest.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.springrest.entites.Courses;
+
+public interface CourseService {
+
+	public List<Courses> getCourses();
+	
+	public Optional<Courses> getSingleCourse(Long coursesId);
+	
+	public Courses addCourse(Courses courses);
+	
+	public Courses updateCourses(Courses courses);
+	
+	public void deleteCourse(Long courseId);
+	
+	public void deleteAllCourses();
+}
