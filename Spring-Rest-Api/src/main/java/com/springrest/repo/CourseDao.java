29f1864a2cc9f7620a6e29a27a2549e4981306c1
sanketschendake <76549372+sanketschendake@users.noreply.github.com>
2023-08-09 12:04:23 +0530

    Delete Spring-Rest-Api directory

--- Spring-Rest-Api/src/main/java/com/springrest/repo/CourseDao.java
+++
@@ -1,9 +0,0 @@
-package com.springrest.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springrest.entites.Courses;
-
-public interface CourseDao extends JpaRepository<Courses, Long>{
-
-}
